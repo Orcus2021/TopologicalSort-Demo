129ca99940f089822a1af10b1e986cfb6691e03c
orcus <orcus20210925@gmail.com>
2023-07-30 11:13:42 +0800

    chore: add styled-components and react-use-measure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 
 function App() {
 	return <div>App</div>;
-=======
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
->>>>>>> 6367b34 (feat: remove unused data)
 }
 
 export default App;
