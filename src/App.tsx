b041fbc5027289ddd46d67277fc4ac0c37b777b5
orcus <orcus20210925@gmail.com>
2023-08-06 15:34:46 +0800

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { THEME } from "./constants/theme";
+import TopologicalPage from "./pages/TopologicalPage";
 
+const GlobalStyle = createGlobalStyle`
+  * {
+	box-sizing: border-box;
+  }
+
+  body {
+	margin: 0;
+	padding: 0;
+  color: white;
+  }
+`;
 function App() {
-	return <div>App</div>;
+  return (
+    <ThemeProvider theme={THEME}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/constants/theme.ts
@@ -0,0 +1,6 @@
+export const THEME = {
+  background: "#101418",
+  primary: "#90caf9",
+  secondary: "#507089",
+  hover: "#1a222a",
+};
